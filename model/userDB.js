27d676cfcc5ece4dd04ce5ec180e65de0bdb7104
Yonela <yonelamnagele1700@gmail.com>
2024-09-06 09:13:35 +0200

    third backend commit

--- model/userDB.js
+++ model/userDB.js
@@ -1,27 +1,27 @@
 import {pool} from '../config/config.js'
 
 const  getUsersDB = async () => {
-    let [data] = await pool.query('SELECT * FROM Users')
+    let [data] = await pool.query('SELECT * FROM users')
     return data
 }
 
-const getUserDB = async (emailAdd) => {
-    let [[data]] = await pool.query('SELECT * FROM Users WHERE emailAdd = ?', [emailAdd])
+const getUserDB = async (id) => {
+    let [[data]] = await pool.query('SELECT * FROM users WHERE userID = ?', [id])
     return data
 }
 
 const insertUserDB = async (firstName, lastName, userAge, gender, emailAdd, userPass, userProfile) => {
     let [data] = await pool.query(`
-        INSERT INTO Users (firstName, lastName, userAge, gender, emailAdd, userPass, userProfile) VALUES (?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO users (firstName, lastName, userAge, gender, emailAdd, userPass, userProfile) VALUES (?, ?, ?, ?, ?, ?, ?)
         `, [firstName, lastName, userAge, gender, emailAdd, userPass, userProfile])
 }
 
 const updateUserDB = async (firstName, lastName, userAge, gender, userRole, emailAdd, userPass, userProfile, id) => {
-     await pool.query('UPDATE Users SET firstName = ?, lastName = ?, userAge = ?, gender = ?, userRole = ?, emailAdd = ?, userPass = ?, userProfile = ? WHERE userID = ?', [firstName, lastName, userAge, gender, userRole, emailAdd, userPass, userProfile, id])
+     await pool.query('UPDATE users SET firstName = ?, lastName = ?, userAge = ?, gender = ?, userRole = ?, emailAdd = ?, userPass = ?, userProfile = ? WHERE userID = ?', [firstName, lastName, userAge, gender, userRole, emailAdd, userPass, userProfile, id])
 }
 
 const deleteUserDB = async (id) => {
-    let [data] = await pool.query('DELETE FROM Users WHERE userID = ?', [id])
+    let [data] = await pool.query('DELETE FROM users WHERE userID = ?', [id])
     return data
 }
 
